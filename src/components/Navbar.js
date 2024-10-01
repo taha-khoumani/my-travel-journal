4e5ed3eb9aba7b5f6c8011d3484d0e2b20790af1
taha-khoumani <khoumanitaha23@gmail.com>
2024-10-01 06:11:00 -0400

    Updates

--- src/components/Navbar.js
+++
@@ -1,8 +0,0 @@
-export default function Navbar(){
-    return(
-        <nav className="nav-bar">
-            <i class="fa-solid fa-earth-africa"></i>
-            <p className="page-name">my travel journal.</p>
-        </nav>
-    )
-}
